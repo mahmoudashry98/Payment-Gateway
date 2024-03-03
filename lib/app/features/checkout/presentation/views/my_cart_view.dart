9d82a67d5aab6578a0ee36a830c7a07433f9ee75
mahmoudashry98 <83143927+mahmoudashry98@users.noreply.github.com>
2024-03-03 07:03:49 +0200

    add paypal

--- lib/app/features/checkout/presentation/views/my_cart_view.dart
+++ lib/app/features/checkout/presentation/views/my_cart_view.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:payment_gateways/app/core/utils/styles.dart';
 import 'package:payment_gateways/app/core/widgets/custom_app_button.dart';
 import 'package:payment_gateways/app/core/widgets/custom_app_bar.dart';
@@ -69,18 +72,70 @@ class MyCartView extends StatelessWidget {
     return CustomButton(
       text: 'Complete Payment',
       onPressed: () {
-        showModalBottomSheet(
-          context: context,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-          builder: (context) {
-            return BlocProvider(
-              create: (context) => PaymentCubit(CheckOutRepoImpl()),
-              child: const PaymentDetailsView(),
-            );
-          },
-        );
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (BuildContext context) => PaypalCheckoutView(
+            sandboxMode: true,
+            clientId: "YOUR CLIENT ID",
+            secretKey: "YOUR SECRET KEY",
+            transactions: const [
+              {
+                "amount": {
+                  "total": "100",
+                  "currency": "USD",
+                  "details": {
+                    "subtotal": "100",
+                    "shipping": "0",
+                    "shipping_discount": 0
+                  }
+                },
+                "description": "The payment transaction description.",
+                "item_list": {
+                  "items": [
+                    {
+                      "name": "Apple",
+                      "quantity": 4,
+                      "price": '10',
+                      "currency": "USD"
+                    },
+                    {
+                      "name": "Pineapple",
+                      "quantity": 5,
+                      "price": '12',
+                      "currency": "USD"
+                    }
+                  ],
+                }
+              }
+            ],
+            note: "Contact us for any questions on your order.",
+            onSuccess: (Map params) async {
+              log("onSuccess: $params");
+              Navigator.pop(context);
+            },
+            onError: (error) {
+              log("onError: $error");
+              Navigator.pop(context);
+            },
+            onCancel: () {
+              print('cancelled:');
+              Navigator.pop(context);
+            },
+          ),
+        ));
       },
+      // onPressed: () {
+      //   showModalBottomSheet(
+      //     context: context,
+      //     shape:
+      //         RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      //     builder: (context) {
+      //       return BlocProvider(
+      //         create: (context) => PaymentCubit(CheckOutRepoImpl()),
+      //         child: const PaymentDetailsView(),
+      //       );
+      //     },
+      //   );
+      // },
     );
   }
 }
